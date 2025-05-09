fd1191cfdb2ba132083a31fed7f02f5826183cd3
SimbaFs <simba.fs@gmail.com>
2025-05-10 00:19:36 +0800

    init

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,17 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { config } from "../site-config";
+import type { AstroGlobal } from "astro";
+
+export async function GET(context: AstroGlobal) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: config.title,
+    description: config.description,
+    site: context.site || "",
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id}/`,
+    })),
+  });
+}
---
+++ src/site-config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  title: "長條貓窩",
+  description: "Welcome to my website!",
+};
