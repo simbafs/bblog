e318537306c8d493303c044f050007aaeac83861
SimbaFs <simba.fs@gmail.com>
2025-05-10 00:23:47 +0800

    move site-config.ts -> site.config.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { config } from "../site-config";
+import { config } from "../site.config";
 import type { AstroGlobal } from "astro";
 
 export async function GET(context: AstroGlobal) {
