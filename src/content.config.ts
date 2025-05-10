e9f2cba97fa8cb2938ffefe1aa93cfd6bdc87800
SimbaFs <simba.fs@gmail.com>
2025-05-10 22:18:26 +0800

    tree output at /blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,13 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
+function removeDupsAndLowerCase(array: string[]) {
+  if (!array.length) return array;
+  const lowercaseItems = array.map((str) => str.toLowerCase());
+  const distinctItems = new Set(lowercaseItems);
+  return Array.from(distinctItems);
+}
+
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
@@ -9,9 +16,11 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
-    pubDate: z.coerce.date(),
+    publishDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
     heroImage: z.string().optional(),
+    draft: z.boolean().default(false),
   }),
 });
 
